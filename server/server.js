07939a5396a1b1ab37d844029c668728605726cd
chirag <chiragtilvani06@gmail.com>
2025-09-20 01:10:55 +0530

    bug fix #4

--- server/server.js
+++ server/server.js
@@ -1,28 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const authRoutes = require('./routes/auth');
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/api/auth', authRoutes);
-
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     app.listen(5000, () => console.log('Server running on http://localhost:5000'));
-//   })
-//   .catch(err => console.error(err));
-
-
-// server.js
-
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -44,10 +19,7 @@ app.use(express.json());  // 📌 This is what parses req.body
 app.use('/api/auth', authRoutes);
 
 // DB Connect
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   app.listen(5000, () => console.log('✅ Server running on http://localhost:5000'));
 })
