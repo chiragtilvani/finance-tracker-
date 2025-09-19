ca0ba2bca8910dd1dd49a49dd350a6ee88761385
chirag <chiragtilvani06@gmail.com>
2025-09-20 01:23:03 +0530

    vercel source

--- server/server.js
+++ server/server.js
@@ -23,4 +23,4 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   app.listen(5000, () => console.log('✅ Server running on http://localhost:5000'));
 })
-.catch(err => console.error('❌ MongoDB Error:', err));
+.catch(err => console.error('❌ MongoDB Error:', err.message));
